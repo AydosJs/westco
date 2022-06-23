009cc69b3b5eb1a754c5e234d5dc8cfd8d47334c
Aydos Sankibaev <aji3385@gmail.com>
2022-06-23 12:31:10 +0500

    sign in and sign up on the navigation

--- src/layout/Navigation.tsx
+++ src/layout/Navigation.tsx
@@ -114,26 +114,21 @@ export default function Navigation({ open, toggleMenu }: Props) {
             {
               !isLoggedIn && (
                 <div className="block lg:hidden">
-                  <Link to="/sign-in">
-                    <li className={`p-2 rounded text-slate-500  flex flex-row flex-nowrap space-x-4 items-center text-sm font-medium hover:text-blue-500 group cursor-pointer`}>
-                      <span>
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                          <path stroke-linecap="round" stroke-linejoin="round" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
-                        </svg>
-                      </span>
-                      <h6 className=" text-md font-medium">Sign-In</h6>
-                    </li>
+                  <Link className="p-2 border rounded py-1 hover:border-blue-400 flex flex-row space-x-2 items-center text-md text-gray-600 font-medium hover:text-blue-500" to="/sign-in">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1" />
+                    </svg>
+                    Sign-In
                   </Link>
-                  <Link to="/register">
-                    <li className={`p-2 rounded text-slate-500  flex flex-row flex-nowrap space-x-4 items-center text-sm font-medium hover:text-blue-500 group cursor-pointer`}>
-                      <span>
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                          <path stroke-linecap="round" stroke-linejoin="round" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
-                        </svg>
-                      </span>
-                      <h6 className=" text-md font-medium">Sign-Up</h6>
-                    </li>
+                  <Link
+                    to="/register"
+                    className="p-2 mt-2 border rounded py-1 hover:border-blue-400 flex flex-row space-x-2 items-center text-md text-gray-600 font-medium hover:text-blue-500">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z" />
+                    </svg>
+                    Sign-Up
                   </Link>
+
                 </div>
               )
             }
